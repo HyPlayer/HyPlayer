8f78eb5bcd6447ff75227cc1b53b5da9deadbd7a
天湖 <3605729117@qq.com>
2021-07-02 12:39:38 +0800

    优化第三方登录 网抑加强了对回调state的验证，导致随机生成的state失效 只能用移动版的网页接口了，不过好处就是不会失效

--- HyPlayer/Pages/ThirdPartyLogin.xaml.cs
+++ HyPlayer/Pages/ThirdPartyLogin.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using HyPlayer.Classes;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,8 +24,11 @@ namespace HyPlayer.Pages
     /// </summary>
     public sealed partial class ThirdPartyLogin : Page
     {
+        private bool IsQQ = false;
+        private bool IsWB = false;
+        private string DefaultUA;
         string codeSerial = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,M,L,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
-        private readonly string QQLoginUri = @"https://graph.qq.com/oauth2.0/show?which=Login&display=pc&client_id=100495085&response_type=code&redirect_uri=https://music.163.com/back/qq&state=";
+        private readonly string QQLoginUri = @"https://graph.qq.com/oauth2.0/show?which=Login&display=pc&client_id=100495085&response_type=code&redirect_uri=https://music.163.com/back/qq";
         private readonly string WXLoginUri = @"https://open.weixin.qq.com/connect/qrconnect?appid=wxe280063f5fb2528a&response_type=code&redirect_uri=https://music.163.com/back/weichat&scope=snsapi_login&state=&lang=zh_CN#wechat_redirect";
         private readonly string WBLoginUri = @"https://api.weibo.com/oauth2/authorize?client_id=301575942&response_type=code&redirect_uri=http://music.163.com/back/weibo&scope=friendships_groups_read,statuses_to_me_read,follow_app_official_microblog&state=";
         public ThirdPartyLogin()
@@ -55,27 +60,91 @@ namespace HyPlayer.Pages
             }
 
             return code;
+
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            UAHelper.SetUserAgent(DefaultUA);
+            ThirdPartyLoginWebview = null;
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+            DefaultUA = UAHelper.GetUserAgent();
+            UAHelper.SetUserAgent("Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; Lumia 950) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063");
             switch (e.Parameter as String)
             {
+
                 case "QQ":
-                    ThirdPartyLoginWebview.Navigate(new Uri(QQLoginUri + CreateRandomState(10, true)));
+                    IsQQ = true;
+                    ThirdPartyLoginWebview.Navigate(new Uri("https://y.music.163.com/m/login"));
                     break;
                 case "WX":
                     ThirdPartyLoginWebview.Navigate(new Uri(WXLoginUri + CreateRandomState(10, false)));
                     break;
                 case "WB":
-                    ThirdPartyLoginWebview.Navigate(new Uri(WBLoginUri + CreateRandomState(10, false) + "###"));
+                    IsWB = true;
+                    ThirdPartyLoginWebview.Navigate(new Uri("https://y.music.163.com/m/login"));
                     break;
             }
         }
 
-        private void ThirdPartyLoginWebview_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+
+
+
+        private async void ThirdPartyLoginWebview_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
         {
-            if (sender.Source.ToString().Contains("music.163.com/back/sns"))
+
+            if (sender.Source.ToString().StartsWith("https://xui.ptlogin2.qq.com"))
+            {
+                Regex match = new Regex("state=[a-zA-Z]{10}");
+                if (match.IsMatch(sender.Source.ToString()))
+                {
+                    UAHelper.SetUserAgent(DefaultUA);
+                    ThirdPartyLoginWebview.Navigate(new Uri(QQLoginUri + "&" + match.Match(sender.Source.ToString())));
+
+                }
+
+            }
+
+            if (sender.Source.ToString().StartsWith("https://api.weibo.com"))
+            {
+
+                await ThirdPartyLoginWebview.InvokeScriptAsync("eval", new[]
+{
+    "document.getElementById('jump_login_url_a').click();"
+});
+
+
+            }
+
+            if (sender.Source.ToString().StartsWith("https://y.music.163.com/m/login")&& !sender.Source.ToString().Contains("code=200"))
+            {
+                await ThirdPartyLoginWebview.InvokeScriptAsync("eval", new[]{
+                    "var x = document.getElementsByClassName('u-terms-checkbox');" +
+                    "x[0].click();"
+});
+                if (IsQQ)
+                {
+                    await ThirdPartyLoginWebview.InvokeScriptAsync("eval", new[]
+{
+    "var x = document.getElementsByClassName('item');" +
+    "x[0].click();"
+});
+                }
+                else if (IsWB)
+                {
+                    await ThirdPartyLoginWebview.InvokeScriptAsync("eval", new[]
+{
+    "var x = document.getElementsByClassName('item');" +
+    "x[1].click();"
+});
+                   
+                }
+            }
+
+            if (sender.Source.ToString().StartsWith("https://music.163.com")|| sender.Source.ToString().Contains("code=200"))
             {
 
                 var cookies = new Windows.Web.Http.Filters.HttpBaseProtocolFilter().CookieManager.GetCookies(new Uri("https://music.163.com"));
