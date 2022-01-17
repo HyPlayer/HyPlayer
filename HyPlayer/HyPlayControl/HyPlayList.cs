HyPlayer/HyPlayer

commit 0f2c566d7f2175be738dc7251050e6b87d821da8

修复部分问题
* 修复码率显示
* 修复本地歌曲播放
+ 新增登录帮助

HyPlayer/HyPlayControl/HyPlayList.cs
@@ -413,7 +413,17 @@ public static class HyPlayList
                         { "br", Common.Setting.audioRate }
                     });
                 if (json["data"]?[0]?["code"]?.ToString() == "200")
+                {
                     playUrl = json["data"][0]["url"]?.ToString();
+                    int bitrate = json["data"][0]["br"]?.ToObject<int>() ?? 0;
+                    string tag;
+                    if (bitrate > 900000)
+                        tag = "无损";
+                    else
+                        tag = $"{bitrate / 1000}K";
+                    if (bitrate == 0) tag = "在线";
+                    Common.BarPlayBar.TbSongTag.Text = tag;
+                }
                 else
                     PlayerOnMediaFailed(Player, null); //传一个播放失败
             }

HyPlayer/Controls/PlayBar.xaml
@@ -108,6 +108,7 @@
                             BorderThickness="1">
                             <TextBlock
                                 Name="TbSongTag"
+                                x:FieldModifier="public"
                                 FontSize="12"
                                 FontWeight="Medium"
                                 Foreground="Red"

HyPlayer/Controls/PlayBar.xaml.cs
@@ -254,8 +254,8 @@ public sealed partial class PlayBar
         foreach (var file in files)
         {
             var folder = await file.GetParentAsync();
-            if (!StorageApplicationPermissions.FutureAccessList.ContainsItem(folder.Path))
-                StorageApplicationPermissions.FutureAccessList.AddOrReplace(file.FolderRelativeId, folder);
+            if (!StorageApplicationPermissions.FutureAccessList.ContainsItem(folder.Path.GetHashCode().ToString()))
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace(folder.Path.GetHashCode().ToString(), folder);
             if (Path.GetExtension(file.Path) == ".ncm")
             {
                 //脑残Music
@@ -459,7 +459,8 @@ public sealed partial class PlayBar
             realSelectSong = false;
             ListBoxPlayList.SelectedIndex = HyPlayList.NowPlaying;
             realSelectSong = true;
-            TbSongTag.Text = HyPlayList.NowPlayingItem.PlayItem.Tag;
+            if (HyPlayList.NowPlayingItem.PlayItem.Tag != "在线")
+                TbSongTag.Text = HyPlayList.NowPlayingItem.PlayItem.Tag;
             Btn_Share.IsEnabled = HyPlayList.NowPlayingItem.ItemType == HyPlayItemType.Netease;
             /*
             verticalAnimation.To = TbSongName.ActualWidth - TbSongName.Tb.ActualWidth;

HyPlayer/Pages/BasePage.xaml
@@ -275,57 +275,73 @@
                     Title="登录代表你同意相关条款"
                     IsClosable="False"
                     IsOpen="True" />
-                <Button
-                    HorizontalAlignment="Center"
-                    Background="Transparent"
-                    Content="第三方账号登录"
-                    Style="{StaticResource ButtonRevealStyle}">
-                    <Button.Flyout>
-                        <Flyout>
-                            <StackPanel
-                                Height="50"
-                                Orientation="Horizontal"
-                                Spacing="10">
-                                <Button
-                                    x:Name="QQLogin"
-                                    MinWidth="48"
-                                    MinHeight="48"
-                                    Click="ThirdPartyLogin_Click"
-                                    CornerRadius="5"
-                                    Style="{StaticResource ButtonRevealStyle}"
-                                    Tag="QQ">
-                                    <Button.Content>
-                                        <Image Source="/Assets/ThirdPartyIcons/QQ.png" Stretch="Fill" />
-                                    </Button.Content>
-                                </Button>
-                                <Button
-                                    x:Name="WXLogin"
-                                    MinWidth="48"
-                                    MinHeight="48"
-                                    Click="ThirdPartyLogin_Click"
-                                    CornerRadius="5"
-                                    Style="{StaticResource ButtonRevealStyle}"
-                                    Tag="WX">
-                                    <Button.Content>
-                                        <Image Source="/Assets/ThirdPartyIcons/WX.png" Stretch="Fill" />
-                                    </Button.Content>
-                                </Button>
-                                <Button
-                                    x:Name="WBLogin"
-                                    MinWidth="48"
-                                    MinHeight="48"
-                                    Click="ThirdPartyLogin_Click"
-                                    CornerRadius="5"
-                                    Style="{StaticResource ButtonRevealStyle}"
-                                    Tag="WB">
-                                    <Button.Content>
-                                        <Image Source="/Assets/ThirdPartyIcons/SinaWB.png" Stretch="Fill" />
-                                    </Button.Content>
-                                </Button>
-                            </StackPanel>
-                        </Flyout>
-                    </Button.Flyout>
-                </Button>
+                <Grid Margin="0,5,0,0" RowSpacing="5">
+                    <Grid.ColumnDefinitions>
+                        <ColumnDefinition />
+                        <ColumnDefinition />
+                    </Grid.ColumnDefinitions>
+                    <Button
+                        Grid.Column="0"
+                        Background="Transparent"
+                        BorderThickness="0"
+                        Content="第三方账号登录">
+                        <Button.Flyout>
+                            <Flyout>
+                                <StackPanel
+                                    Height="50"
+                                    Orientation="Horizontal"
+                                    Spacing="10">
+                                    <Button
+                                        x:Name="QQLogin"
+                                        MinWidth="48"
+                                        MinHeight="48"
+                                        Click="ThirdPartyLogin_Click"
+                                        CornerRadius="5"
+                                        Style="{StaticResource ButtonRevealStyle}"
+                                        Tag="QQ">
+                                        <Button.Content>
+                                            <Image Source="/Assets/ThirdPartyIcons/QQ.png" Stretch="Fill" />
+                                        </Button.Content>
+                                    </Button>
+                                    <Button
+                                        x:Name="WXLogin"
+                                        MinWidth="48"
+                                        MinHeight="48"
+                                        Click="ThirdPartyLogin_Click"
+                                        CornerRadius="5"
+                                        Style="{StaticResource ButtonRevealStyle}"
+                                        Tag="WX">
+                                        <Button.Content>
+                                            <Image Source="/Assets/ThirdPartyIcons/WX.png" Stretch="Fill" />
+                                        </Button.Content>
+                                    </Button>
+                                    <Button
+                                        x:Name="WBLogin"
+                                        MinWidth="48"
+                                        MinHeight="48"
+                                        Click="ThirdPartyLogin_Click"
+                                        CornerRadius="5"
+                                        Style="{StaticResource ButtonRevealStyle}"
+                                        Tag="WB">
+                                        <Button.Content>
+                                            <Image Source="/Assets/ThirdPartyIcons/SinaWB.png" Stretch="Fill" />
+                                        </Button.Content>
+                                    </Button>
+                                </StackPanel>
+                            </Flyout>
+                        </Button.Flyout>
+                    </Button>
+                    <Button
+                        x:Name="BtnLoginHelp"
+                        Grid.Column="1"
+                        HorizontalAlignment="Right"
+                        Background="Transparent"
+                        BorderThickness="0"
+                        Click="BtnLoginHelp_Click">
+                        无法登录?
+                    </Button>
+                </Grid>
+
 
             </StackPanel>
         </ContentDialog>

HyPlayer/Pages/BasePage.xaml.cs
@@ -781,4 +781,9 @@ public sealed partial class BasePage : Page
             Common.AddToTeachingTipLists("删除失败", ex.Message);
         }
     }
+
+    private void BtnLoginHelp_Click(object sender, RoutedEventArgs e)
+    {
+        _ = Launcher.LaunchUriAsync(new Uri(@"https://blog.kengwang.com.cn/hyplayer-login-help"));
+    }
 }

commit 3b7e1680a3dcd70ba482f8c4d7d7ff7ded452b39

Fixed 无法播放第二首歌
Fixed a bug caused by 0f2c566d7f2175be738dc7251050e6b87d821da8

HyPlayer/HyPlayControl/HyPlayList.cs
@@ -422,7 +422,11 @@ public static class HyPlayList
                     else
                         tag = $"{bitrate / 1000}K";
                     if (bitrate == 0) tag = "在线";
-                    Common.BarPlayBar.TbSongTag.Text = tag;
+                    Common.Invoke(() =>
+                    {
+                        Common.BarPlayBar.TbSongTag.Text = tag;
+                    });
+                    
                 }
                 else
                     PlayerOnMediaFailed(Player, null); //传一个播放失败