6ec1b904acce4cf68b97e4fbdc7cf488cf6a5f8c
Raspberry-Monster <lele2194512339@outlook.com>
2023-06-04 18:37:15 +0800

    [Improvements] 优化CloudMusicAPI内存表现

--- HyPlayer/MainPage.xaml.cs
+++ HyPlayer/MainPage.xaml.cs
@@ -29,8 +29,12 @@ public sealed partial class MainPage
     {
         Common.PageMain = this;
         Common.ncapi.RealIP = Setting.GetSettings<string>("xRealIp", null);
-        Common.ncapi.Proxy = new WebProxy(Setting.GetSettings<string>("neteaseProxy", null));
-        Common.ncapi.UseProxy = !(ApplicationData.Current.LocalSettings.Values["neteaseProxy"] is null);
+        var proxy = Setting.GetSettings<string>("neteaseProxy", null);
+        if(!string.IsNullOrEmpty(proxy))
+        {
+            Common.ncapi.UseProxy = true;
+            Common.ncapi.Proxy = new WebProxy(proxy);
+        }
         Common.ncapi.UseHttp = Setting.GetSettings<bool>("UseHttp", false);
         StaticSource.PICSIZE_AUDIO_PLAYER_COVER = Common.Setting.highQualityCoverInSMTC ? "1024y1024" : "640x640";
         if (Common.Setting.uiSound)
