46b0754139bff6a896517b452253d456d19f5469
Aesthed <aaaaaaccd@gmail.com>
2023-07-07 13:03:03 +0800

    初步实现

--- HyPlayer/Controls/PlayBar.xaml.cs
+++ HyPlayer/Controls/PlayBar.xaml.cs
@@ -24,6 +24,7 @@ using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
