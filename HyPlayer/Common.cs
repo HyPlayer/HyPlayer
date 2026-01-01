0b624e04f849ff3e8970cc1b4c039f02b0b2a1d9
Kengwang <github@kengwang.com.cn>
2026-01-02 01:15:40 +0800

    [FOR-TEST] 强制某些设置项

--- HyPlayer/Common.cs
+++ HyPlayer/Common.cs
@@ -498,7 +498,7 @@ namespace HyPlayer
 
         public BackgroundType expandedPlayerBackgroundType
         {
-            get => GetSettings(nameof(expandedPlayerBackgroundType), BackgroundType.CoverBlur);
+            get => BackgroundType.Isolation;
             set
             {
                 ApplicationData.Current.LocalSettings.Values[nameof(expandedPlayerBackgroundType)] = (int)value;
@@ -1679,7 +1679,7 @@ namespace HyPlayer
         }
         public double IsolationFPS
         {
-            get => GetSettings(nameof(IsolationFPS), 24d);
+            get => Math.Max(GetSettings(nameof(IsolationFPS), 60d), 60d);
             set
             {
                 ApplicationData.Current.LocalSettings.Values[nameof(IsolationFPS)] = value;
