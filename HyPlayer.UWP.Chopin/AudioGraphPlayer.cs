1907846318149e229d3d7086db3174ae09b09fe2
Raspberry Kan <lele2194512339@outlook.com>
2026-03-02 09:38:09 +0800

    Revert "bugfix: 修复部分情况下FrameOutputNode未被正确添加或移除的Bug"
    This reverts commit 4f23b63da182f72ac69d401716ad7b2a7bcb3641.

--- HyPlayer.UWP.Chopin/AudioGraphPlayer.cs
+++ HyPlayer.UWP.Chopin/AudioGraphPlayer.cs
@@ -159,7 +159,6 @@ namespace HyPlayer.UWP.Chopin.Abstractions.Models
                 
 
                 oldPlayer.Stop();
-                oldPlayer.QuantumProcessed -= GraphOnQuantumProcessed;
 
                 // 创建新的输出节点
                 var deviceNodeCreateResult = await newPlayer.CreateDeviceOutputNodeAsync();
@@ -168,7 +167,7 @@ namespace HyPlayer.UWP.Chopin.Abstractions.Models
                 var newOutputNode = deviceNodeCreateResult.DeviceOutputNode;
                 newOutputNode.OutgoingGain = oldOutputNode.OutgoingGain;
                 
-                var frameOutputResult = newPlayer.CreateFrameOutputNode(_frameEncodingProperties);
+                var frameOutputResult = newPlayer.CreateFrameOutputNode();
                 _frameOutputNode = frameOutputResult;
 
                 // 转移所有播放源
@@ -202,7 +201,6 @@ namespace HyPlayer.UWP.Chopin.Abstractions.Models
                     newNode.PlaybackSpeedFactor = factor;
                     newNode.OutgoingGain = gain;
                     newNode.AddOutgoingConnection(newOutputNode);
-                    newNode.AddOutgoingConnection(_frameOutputNode);
 
                     // 应用效果
                     foreach (var effect in effects)
@@ -462,7 +460,6 @@ namespace HyPlayer.UWP.Chopin.Abstractions.Models
             {
                 source.Value.MediaSourceCompleted -= OnMediaSourceCompleted;
                 source.Value.RemoveOutgoingConnection(_outputNode);
-                source.Value.RemoveOutgoingConnection(_frameOutputNode);
                 source.Value.Dispose();
             }
             _audioInputNodes.Clear();
